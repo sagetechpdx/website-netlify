f8e689ba3267aafc677978265649a4e48ae8c992
sagetechpdx <info@sagetech.support>
2025-01-14 13:52:43 -0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
+// Import the Netlify adapter
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://website-alpha-gold.vercel.app/",
+  site: "https://website-sagetech.netlify.app/",
   integrations: [tailwind()], 
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
