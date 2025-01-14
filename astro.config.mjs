ee31d24c622c97ecf3039b673e75ec843e249edd
sagetechpdx <info@sagetech.support>
2025-01-14 13:55:16 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://website-sagetech.netlify.app/",
+  site: "https://sagetech.netlify.app/",
   integrations: [tailwind()], 
   output: 'server',
   adapter: netlify(),
